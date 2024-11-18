3d401ec658b12742a0ac0e8a5c2c16847095a9f6
prajula255 <prajulasouparnika3@gmail.com>
2024-11-18 12:01:42 +0530

    added todo list

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import TextInput from './components/text-input';
 
 function App() {
   const [todo, setTodo] = useState([])
+  const [input, setInput] = useState("Test")
+
+  function handleAdd() {
+    setTodo([...todo, input])
+  }
   return (
     <div className="container">
       <Heading1 value={"Todo list"} className="text-center" />
       <div className="d-flex gap-2">
         <TextInput />
-        <Button label={"Add"} />
+        <Button label={"Add"} onClick={handleAdd} />
       </div>
-      <div className="mt-4">
+      <div className="mt-4 grid row ">
         {
           todo.length > 0 ?
             todo.map((item, index) => (
-        <Card />
-        )):
-        (
-        <div>
-          <p>nothing to show</p>
-        </div>
-        )
-      }
+              <div className="col-2 p-2" >
+                <Card key={index} todo={item} />
+              </div>
+            )) :
+            (
+              <div>
+                <p>nothing to show</p>
+              </div>
+            )
+        }
 
       </div>
     </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 function Card() {
     return (
         <>
-            <div className="card" style={{ width: "18rem" }}>
+            <div className="card w-full" >
                 <div class="card-body">
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <p class="card-text">test</p>
                     <a href="#" class="btn btn-primary">done</a>
                     <a href="#" class="ms-2 btn btn-primary">delete</a>
 
